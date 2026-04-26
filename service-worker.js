47f901804e55c4ceedec737da662c11798c7c58b
mohak <mohakchoubey@gmail.com>
2026-04-26 18:30:05 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "gatividhiya-v1";
+const ASSETS = [
+  "/gatividhiya/",
+  "/gatividhiya/index.html",
+  "/gatividhiya/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => caches.match("/gatividhiya/index.html"));
+    })
+  );
+});
